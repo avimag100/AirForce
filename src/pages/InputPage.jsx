f181b0159f0f1f257debb7f196d6a7aeec132d6c
avi <avimagedman@gmail.com>
2024-03-31 10:32:14 +0300

    crated a input page for the page to input the degrees and the rest of the data

---
+++ src/pages/InputPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Input from '../components/Input';
+import { IoSend } from "react-icons/io5";
+import Circle from '../components/Circle';
+
+const InputPage = () => {
+  const [inputValues, setInputValues] = useState({
+    Altitude: '',
+    HIS: '',
+    ADI: ''
+  });
+
+  const handleInputChange = (type, value) => {
+    setInputValues(prevState => ({
+      ...prevState,
+      [type]: value
+    }));
+  };
+
+  const handleSubmint = () => {
+    console.log('Input values:', inputValues);
+  };
+
+  return (
+    <div className='flex'>
+      <div>
+        <Input type="Altitude" maxValue={3000} onChange={(value) => handleInputChange('Altitude', value)} />
+        <Input type="HIS" maxValue={360} onChange={(value) => handleInputChange('HIS', value)} />
+        <Input type="ADI" maxValue={100} onChange={(value) => handleInputChange('ADI', value)} />
+
+        <button className='self-end' onClick={handleSubmint}><IoSend /></button>
+        <Circle/>
+      </div>
+    </div>
+  );
+};
+
+export default InputPage;
